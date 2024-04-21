72f48c099f5f6cb4d5ae845b4efa457be419e416
Merezha Roman <rmerezha@gmail.com>
2024-04-21 17:10:02 +0300

    Add entity/User.java

--- src/main/java/space/neptuxo/entity/User.java
+++ src/main/java/space/neptuxo/entity/User.java
@@ -1,4 +1,18 @@
 package space.neptuxo.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class User {
+
+    private long id;
+    private String username;
+    private String email;
+    private String passwd;
+
 }
