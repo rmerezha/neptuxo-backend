57389efc5f0929bfd1c6c36657cb057191cbc9cd
Merezha Roman <rmerezha@gmail.com>
2024-04-21 15:28:45 +0300

    Add ddl_scripts.sql

---
+++ ddl_scripts.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id       BIGSERIAL PRIMARY KEY,
+    username VARCHAR(128) NOT NULL UNIQUE,
+    email    VARCHAR(128) NOT NULL UNIQUE,
+    passwd   VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS product
+(
+    id          BIGSERIAL PRIMARY KEY,
+    user_id     BIGINT REFERENCES users (id) ON DELETE CASCADE NOT NULL,
+    description VARCHAR(512),
+    type        VARCHAR(128)                                   NOT NULL,
+    count       INT                                            NOT NULL CHECK (count >= 0)
+
+);
+
+CREATE TABLE IF NOT EXISTS preference
+(
+    user_id BIGINT REFERENCES users (id),
+    type    VARCHAR(128),
+    PRIMARY KEY (user_id, type)
+);
+
+CREATE TABLE IF NOT EXISTS orders
+(
+    id         VARCHAR(128) PRIMARY KEY,
+    product_id BIGINT REFERENCES product (id) NOT NULL,
+    user_id    BIGINT REFERENCES users (id)   NOT NULL,
+    address    VARCHAR(256)                   NOT NULL,
+    type       VARCHAR(128)                   NOT NULL
+);
