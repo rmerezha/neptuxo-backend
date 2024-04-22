e5a59b45f4468464a5dbce368f2fe0b9f5c58b32
Merezha Roman <rmerezha@gmail.com>
2024-04-22 17:04:00 +0300

    Update test/util/PropertiesUtilTest.java

--- src/test/java/space/neptuxo/util/PropertiesUtilTest.java
+++ src/test/java/space/neptuxo/util/PropertiesUtilTest.java
@@ -2,20 +2,34 @@ package space.neptuxo.util;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class PropertiesUtilTest {
 
-    @Test
-    public void testGetProperty() {
-        assertAll(
-                () -> assertEquals("123", PropertiesUtil.get("test.key1")),
-                () -> assertEquals("", PropertiesUtil.get("test.key2"))
-                );
+    @ParameterizedTest
+    @MethodSource("getPropertiesData")
+    public void get(String key, String expectedValue) {
+
+        String actualValue = PropertiesUtil.get(key);
+
+        assertEquals(expectedValue, actualValue);
+
     }
 
+    private static Stream<Arguments> getPropertiesData() {
+        return Stream.of(
+                Arguments.of("test.key1", "123"),
+                Arguments.of("test.key2", ""),
+                Arguments.of("test.key3", null)
+        );
+    }
 
 
 
