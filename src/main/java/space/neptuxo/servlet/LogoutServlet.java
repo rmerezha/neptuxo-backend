81f792dd14a0b5b3f51444d2de75e76c87062208
Merezha Roman <rmerezha@gmail.com>
2024-04-25 18:42:33 +0300

    Add /logout servlet

---
+++ src/main/java/space/neptuxo/servlet/LogoutServlet.java
@@ -0,0 +1,21 @@
+package space.neptuxo.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import space.neptuxo.util.JsonBuilder;
+import space.neptuxo.util.Status;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.getOutputStream().write(new JsonBuilder().setStatus(Status.SUCCESS).build());
+    }
+}
