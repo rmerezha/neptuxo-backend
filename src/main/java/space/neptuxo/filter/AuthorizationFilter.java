3ba9ba099415a3b52549cae7aa615d6f8fa27523
Merezha Roman <rmerezha@gmail.com>
2024-04-26 00:14:05 +0300

    Refactor AuthorizationFilter

--- src/main/java/space/neptuxo/filter/AuthorizationFilter.java
+++ src/main/java/space/neptuxo/filter/AuthorizationFilter.java
@@ -3,11 +3,13 @@ package space.neptuxo.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import space.neptuxo.util.Error;
 import space.neptuxo.util.JsonBuilder;
 import space.neptuxo.util.Status;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.Set;
 
@@ -17,7 +19,7 @@ public class AuthorizationFilter implements Filter {
     private static final Set<String> PUBLIC_PATHS = Set.of(
             "/search",
             "/account"
-            );
+    );
 
     private static final Set<String> AUTHENTICATION_PATHS = Set.of(
             "/registration",
@@ -28,14 +30,22 @@ public class AuthorizationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         var uri = ((HttpServletRequest) servletRequest).getRequestURI();
-        boolean isLoggedIn = isLoggedIn(servletRequest);
-        if (isPublicPath(uri) || (!isLoggedIn && isAuthenticationPath(uri)) || isLoggedIn ) {
+        if (isAllowedAccess(uri, (HttpServletRequest) servletRequest)) {
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
-            servletResponse.getOutputStream().write(new JsonBuilder().setStatus(Status.FAIL).setErrors(List.of(Error.FORBIDDEN)).build());
+            sendForbiddenResponse((HttpServletResponse)servletResponse);
         }
     }
 
+    private boolean isAllowedAccess(String uri, HttpServletRequest request) {
+        return isPublicPath(uri) || (!isLoggedIn(request) && isAuthenticationPath(uri)) || isLoggedIn(request);
+    }
+
+    private void sendForbiddenResponse(HttpServletResponse response) throws IOException {
+        try (OutputStream outputStream = response.getOutputStream()) {
+            response.getOutputStream().write(new JsonBuilder().setStatus(Status.FAIL).setErrors(List.of(Error.FORBIDDEN)).build());
+        }
+    }
     private boolean isPublicPath(String uri) {
         return PUBLIC_PATHS.stream().anyMatch(uri::startsWith);
     }
